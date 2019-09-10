b1b9636af27d2e05068972d90f013a997ea9e612
Stackingrule <messageswitching@gmail.com>
2019-09-10 23:13:39 +0800

    add 27

--- java/26.java
+++ java/26.java
@@ -1,5 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+    	int n = nums.length;
+    	int i = 0, j = 0;
+    	while(++j < n) {
+    		if(nums[i] != nums[j])
+    			nums[++i] = nums[j];
+    	}
+        return i + 1;
     }
-}
+};
