1c94dc8c30f1d07a90b74e74438f429faa68c2a1
Stackingrule <messageswitching@gmail.com>
2019-10-11 23:24:04 +0800

    add 237

--- 腾讯精选练习(50题)/141.cpp
+++ 腾讯精选练习(50题)/141.cpp
@@ -9,9 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {  
-    	ListNode* fast;
-    	ListNode* slow;
-    	
-        
+    	if(head == NULL)
+    		return false;
+    	if(head->next == NULL)
+    		return false;
+
+    	ListNode* slow = head;
+    	ListNode* fast = head->next;
+
+    	while(fast != slow) {
+    		if(fast->next == NULL)
+    			return false;
+    		if(fast->next->next == NULL)
+    			return false;
+
+    		fast = fast->next->next;
+    		slow = slow->next;
+    	}
+    	return true;        
     }
 }; 
