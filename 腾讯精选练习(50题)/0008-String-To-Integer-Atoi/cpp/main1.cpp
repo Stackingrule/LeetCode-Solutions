1cf1b5631d3a843076ab375f3e0208453f262a2e
Stackingrule <messageswitching@gmail.com>
2019-10-31 22:23:29 +0800

    add 0008

--- 腾讯精选练习(50题)/0008-String-To-Integer-Atoi/cpp/main1.cpp
+++ 腾讯精选练习(50题)/0008-String-To-Integer-Atoi/cpp/main1.cpp
@@ -1,35 +1,21 @@
 class Solution {
-private:
-	bool isValid(string str) {
-		if(str[0] != '-' && (str[0] < '0') && str[0] > '9') {
-			return false;
-		}
 
-		if(str[0] == '-' && str.length() == 1 || str[1] == '0') {
-			return false;
-		}
-
-		if(str[0] == '0' && str.length() > 1) {
-			return false;
-		}
-
-		for(int i = 1; i < str.length(); i++) {
-			if(str[i] < '0' || str[i] > '9') {
-				return false;
-			}
-		}
-		return true;
-	}
 public:
     int myAtoi(string str) {
-        if(str == NULL || str == " ") {
-        	return 0;
-        }
 
-        if(!isValid(str)) {
-        	return 0;
+        if (str.empty()) return 0;
+        int sign = 1, base = 0, i = 0, n = str.size();
+        while (i < n && str[i] == ' ') ++i;
+        if (i < n && (str[i] == '+' || str[i] == '-')) {
+            sign = (str[i++] == '+') ? 1 : -1;
         }
-
+        while (i < n && str[i] >= '0' && str[i] <= '9') {
+            if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            base = 10 * base + (str[i++] - '0');
+        }
+        return base * sign;
         
     }
 };
