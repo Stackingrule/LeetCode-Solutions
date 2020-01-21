4dfe5738a83f6981cd80ad942533521fc63e01ca
Stackingrule <messageswitching@gmail.com>
2020-01-21 17:32:39 +0800

    add main1.cpp & main1.java

---
+++ codes/0141-Linked-List-Cycle/Java/main1.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+    	Set<ListNode> node_set = new HashSet<>();
+    	while (head != null) {
+    		if (node_set.contains(head)) {
+    			return true;
+    		}
+    		node_set.add(head);
+    		head = head.next;
+    	}
+        return false;
+    }
+}
