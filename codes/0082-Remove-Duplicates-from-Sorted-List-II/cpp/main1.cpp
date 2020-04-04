8f6f2f2dcd4d16b017fe92b0d17c15f6d5470526
Stackingrule <messageswitching@gmail.com>
2020-04-04 11:23:01 +0800

    add main1.cpp

---
+++ codes/0082-Remove-Duplicates-from-Sorted-List-II/cpp/main1.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *dummy = new ListNode(-1), *pre = dummy;
+        dummy->next = head;
+        while (pre->next) {
+        	ListNode *cur = pre->next;
+        	while (cur->next && cur->next->val == cur->val) {
+        		cur = cur->next;
+        	}
+        	if (cur != pre->next) pre->next = cur->next;
+        	else pre = pre->next;
+        }
+        return dummy->next;
+    }
+};
