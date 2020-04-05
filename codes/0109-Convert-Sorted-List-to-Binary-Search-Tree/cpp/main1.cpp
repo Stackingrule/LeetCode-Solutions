263fc367dd8e8fe1c262f46636a7e47a5ece3f44
Stackingrule <messageswitching@gmail.com>
2020-04-05 10:32:01 +0800

    add main1.cpp

---
+++ codes/0109-Convert-Sorted-List-to-Binary-Search-Tree/cpp/main1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return new TreeNode(head->val);
+        ListNode *fast = head, *slow = head, *last = slow;
+        while (fast->next && fast->next->next) {
+        	last = slow;
+        	slow = slow->next;
+        	fast = fast->next->next;
+        }
+        fast = slow->next;
+        last->next = nullptr;
+        TreeNode *cur = new TreeNode(slow->val);
+        if (head != slow) cur->left = sortedListToBST(head);
+        cur->right = sortedListToBST(fast);
+        return cur; 
+    }
+};
