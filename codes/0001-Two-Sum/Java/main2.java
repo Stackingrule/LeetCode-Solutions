15df428348b16b1896d58abe481c8981dc28335f
Stackingrule <messageswitching@gmail.com>
2020-04-14 09:27:25 +0800

    add main2.java

---
+++ codes/0001-Two-Sum/Java/main2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; ++i) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            int t = target - nums[i];
+            if (map.containsKey(t) && map.get(t) != i) {
+                res[0] = i;
+                res[1] = map.get(t);
+                break;
+            }
+        }
+        return res;
+    }
+}
