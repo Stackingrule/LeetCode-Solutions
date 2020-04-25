af0f1b6427cad5f672b4b100e54786683641fec0
Stackingrule <messageswitching@gmail.com>
2020-04-25 09:55:00 +0800

    main1.java

---
+++ codes/0054-Spiral-Matrix/Java/main1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0) return res;
+        int m = matrix.length, n = matrix[0].length;
+        int up = 0, down = m - 1, left = 0, right = n - 1;
+        while (true) {
+        	for (int j = left; j <= right; ++j) res.add(matrix[up][j]);
+    		if (++up > down) break;
+    		for (int i = up; i <= down; ++i) res.add(matrix[i][right]);
+			if (--right < left) break;
+			for (int j = right; j >= left; --j) res.add(matrix[down][j]);
+			if (--down < up) break;
+			for (int i = down; i >= up; --i) res.add(matrix[i][left]);
+			if (++left > right) break;
+        }
+        return res;
+    }
+}
