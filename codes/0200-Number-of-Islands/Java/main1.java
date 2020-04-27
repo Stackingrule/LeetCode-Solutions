1aac452853fd26e504379ceccb144cd94db97d4b
Stackingrule <messageswitching@gmail.com>
2020-04-27 08:26:30 +0800

    main1.java

---
+++ codes/0200-Number-of-Islands/Java/main1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+    	int m = grid.length;
+    	if (m == 0) return 0;
+    	int n = grid[0].length;
+    	int res = 0;
+    	for (int y = 0; y < m; ++y) {
+    		for (int x = 0; x < n; ++x) {
+    			if (grid[y][x] == '1')
+    				res++;
+    				dfs(grid, x, y, m, n);
+    		}
+    	}
+        return res;
+    }
+
+    private
+    void dfs(char[][] grid, int x, int y, int m, int n) {
+    	if (x < 0 || y < 0 || x >= n || y >= m || grid[y][x] == '0')
+    		return;
+    	grid[y][x] = '0';
+    	dfs(grid, x + 1, y, m, n);
+    	dfs(grid, x - 1, y, m, n);
+    	dfs(grid, x, y + 1, m, n);
+    	dfs(grid, x, y - 1, m, n);
+    }
+}
