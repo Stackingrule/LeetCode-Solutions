34e8c1016eadf2e7241d0805ca8126886e5f6c30
Stackingrule <messageswitching@gmail.com>
2020-05-01 20:51:26 +0800

    add main2.cpp

---
+++ codes/0230-Kth-Smallest-Element-in-a-BST/cpp/main2.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        return kthSmallestDFS(root, k);
+    }
+
+    int kthSmallestDFS(TreeNode* root, int &k) {
+        if (!root) return -1;
+        int val = kthSmallestDFS(root->left, k);
+        if (k == 0) return val;
+        if (--k == 0) return root->val;
+        return kthSmallestDFS(root->right, k);
+    }
+};
