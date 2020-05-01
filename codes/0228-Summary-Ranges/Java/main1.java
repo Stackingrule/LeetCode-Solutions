646afd8e0bf5999ee16e77fdd16b080bd8dca146
Stackingrule <messageswitching@gmail.com>
2020-05-01 19:24:58 +0800

    add main1.java

---
+++ codes/0228-Summary-Ranges/Java/main1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        int i = 0, n = nums.length;
+        while (i < n) {
+        	int j = 1;
+        	while (i + j < n && nums[i + j] - nums[i] == j) ++j;
+        	res.add(j <= 1 ? Integer.toString(nums[i]) : Integer.toString(nums[i]) + "->" + Integer.toString(nums[i + j - 1]));
+        	i += j;
+        }
+        return res;
+    }
+}
