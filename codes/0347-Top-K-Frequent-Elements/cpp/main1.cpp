9116098fd6afe36405d43fbe4bbdfa2a344c75b2
Stackingrule <messageswitching@gmail.com>
2020-05-03 11:23:29 +0800

    add main1.cpp

---
+++ codes/0347-Top-K-Frequent-Elements/cpp/main1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        priority_queue<pair<int, int>> q;
+        vector<int> res;
+        for (auto num : nums) {
+        	++m[num];
+        }
+        for (auto it : m) {
+        	q.push({it.second, it.first});
+        }
+        for (int i = 0; i < k; ++i) {
+        	res.push_back(q.top().second); q.pop();
+        }
+        return res;
+    }
+};
