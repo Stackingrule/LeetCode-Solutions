64949113045d80eb370b5b659fba00b62dcf38cc
Stackingrule <messageswitching@gmail.com>
2020-05-04 16:31:41 +0800

    add main1.cpp

---
+++ codes/0494-Target-Sum/cpp/main1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int res = 0;
+        helper(nums, S, 0, res);
+        return res;
+    }
+
+    void helper(vector<int> &nums, long S, int start, int &res) {
+    	if (start >= nums.size()) {
+    		if (S == 0) ++res;
+    		return;
+    	}
+    	helper(nums, S - nums[start], start + 1, res);
+    	helper(nums, S + nums[start], start + 1, res);
+    }
+};
