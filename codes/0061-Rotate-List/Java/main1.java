4484e6f6b720bd53cae4cb150a44be752bed468e
Stackingrule <messageswitching@gmail.com>
2020-06-01 22:29:09 +0800

    add main1.java

---
+++ codes/0061-Rotate-List/Java/main1.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+        int n = 0;
+        ListNode cur = head;
+        while (cur != null) {
+        	++n;
+        	cur = cur.next;
+        }
+
+        k %= n;
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < k; ++i) {
+        	if (fast != null) {
+        		fast = fast.next;
+        	}
+        }
+        while (fast.next != null) {
+        	fast = fast.next;
+        	slow = slow.next;
+        }
+        fast.next = head;
+        fast = slow.next;
+        slow.next = null;
+        return fast;
+    }
+}
