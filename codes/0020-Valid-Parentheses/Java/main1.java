5729e45467c8543fa87a0c50ba80df4093f6555f
Stackingrule <messageswitching@gmail.com>
2020-06-01 16:55:03 +0800

    add main1.java

---
+++ codes/0020-Valid-Parentheses/Java/main1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); ++i) {
+        	if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') stack.push(s.charAt(i));
+        	else {
+        		if (stack.isEmpty()) return false;
+        		if (s.charAt(i) == ')' && stack.peek() != '(') return false;
+        		if (s.charAt(i) == ']' && stack.peek() != '[') return false;
+        		if (s.charAt(i) == '}' && stack.peek() != '{') return false;
+        		stack.pop();
+        	}
+        }
+        return stack.isEmpty();
+    }
+}
