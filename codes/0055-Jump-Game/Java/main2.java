ec1bb33ad75670c26ef421a5b8800a2f6e227faa
Stackingrule <messageswitching@gmail.com>
2020-06-01 22:18:29 +0800

    add main2.java

---
+++ codes/0055-Jump-Game/Java/main2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length, reach = 0;
+        for (int i = 0; i < n; ++i) {
+        	if (i > reach || reach >= n - 1) break;
+        	reach = Math.max(reach, i + nums[i]);
+        }
+        return reach >= n - 1;
+    }
+}
