244bd97a81bcbf6d5d1e5c9debf224d11c3a3edd
Stackingrule <messageswitching@gmail.com>
2020-06-02 10:21:23 +0800

    add main1.java

---
+++ codes/0063-Unique-Paths-II/Java/main1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
+        	return 0;
+        }
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;
+        for (int i = 1; i <= m; ++i) {
+        	for (int j = 1; j <= n; ++j) {
+        		if (i == 1 && j == 1) continue;
+        		if (obstacleGrid[i - 1][j - 1] == 1) dp[i][j] = 0;
+        		else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        	}
+        }
+        return dp[m][n];
+    }
+}
