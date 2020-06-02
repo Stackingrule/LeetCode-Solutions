2d03bf03926ea62a60f7c8050548eaeec1840cee
Stackingrule <messageswitching@gmail.com>
2020-06-02 10:47:15 +0800

    add main1.java

---
+++ codes/0096-Unique-Binary-Search-Trees/Java/main1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numTrees(int n) {
+        long res = 1;
+        for (int i = n + 1; i <= 2 * n; ++i) {
+        	res = res * i / (i - n);
+        }
+        return (int)(res / (n + 1));
+    }
+}
