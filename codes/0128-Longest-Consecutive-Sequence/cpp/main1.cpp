239e1d7be92b646508b2fa2cfd0e1bb53f061ac4
Stackingrule <messageswitching@gmail.com>
2020-06-05 16:30:10 +0800

    add main1.cpp

--- codes/0128-Longest-Consecutive-Sequence/cpp/main1.cpp
+++ codes/0128-Longest-Consecutive-Sequence/cpp/main1.cpp
@@ -1,23 +1,16 @@
-// Time Limit Exceeded	
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-    	unordered_set<int> my_set;
-    	for(auto i : nums) my_set.insert(i);
-		int longet = 0;
-
-		for(auto i : nums) {
-			int length =  1;
-			for(int j = i - 1; my_set.find(i) != my_set.end(); --j) {
-				my_set.erase(j);
-				++length;
-			}
-			for(int j = i + 1; my_set.find(i) != my_set.end(); ++j) {
-				my_set.erase(j);
-				++length;
-			}
-			longet = max(longet, length);
-		}
-		return longet;
+    	int res = 0;
+    	unordered_set<int> set{nums.begin(), nums.end()};
+    	for (int num : nums) {
+    		if (!set.count(num)) continue;
+    		set.erase(num);
+    		int pre = num - 1, next = num + 1;
+    		while (set.count(pre)) set.erase(pre--);
+    		while (set.count(next)) set.erase(next++);
+    		res = max(res, next - pre - 1);
+    	}
+    	return res;
     }
 };
