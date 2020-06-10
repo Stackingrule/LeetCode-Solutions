c0581363da684a07b01a1729762545b723d15501
Stackingrule <messageswitching@gmail.com>
2020-06-10 09:32:21 +0800

    add main2.java

---
+++ codes/0462-Minimum-Moves-to-Equal-Array-Elements-II/Java/main2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+    	Arrays.sort(nums);
+    	int res = 0, mid = nums[nums.length / 2];
+    	for (int num : nums) {
+    		res += Math.abs(num - mid);
+    	}
+    	return res;
+    }
+}
