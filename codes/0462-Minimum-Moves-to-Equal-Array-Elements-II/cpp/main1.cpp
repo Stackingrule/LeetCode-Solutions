8bab25974b4a6fb48527eaef1a2976662ae1f1c8
Stackingrule <messageswitching@gmail.com>
2020-06-10 09:33:17 +0800

    add main1.cpp

---
+++ codes/0462-Minimum-Moves-to-Equal-Array-Elements-II/cpp/main1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int res = 0, i = 0, j = nums.size() - 1;
+        sort(nums.begin(), nums.end());
+        while (i < j) {
+        	res += nums[j--] - nums[i++];
+        }
+
+        return res;
+    }
+};
