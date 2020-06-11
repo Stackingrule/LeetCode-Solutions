887d2ceb6fb8f90d59c62fd48bef0579cc47ffec
Stackingrule <messageswitching@gmail.com>
2020-06-11 09:08:20 +0800

    add main1.cpp

---
+++ codes/0477-Total-Hamming-Distance/cpp/main1.cpp
@@ -0,0 +1,17 @@
+/*
+* 只要统计出每一位的1的个数即可
+*/
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int res = 0, n = nums.size();
+        for (int i = 0; i < 32; ++i) {
+        	int cnt = 0;
+        	for (int num : nums) {
+        		if (num & (1 << i)) ++cnt;
+        	}
+        	res += cnt * (n - cnt);
+        }
+        return res;
+    }
+};
