faa81aeb06aaf517e02b51631fbc04b1c452b5a1
Stackingrule <messageswitching@gmail.com>
2020-06-21 20:18:11 +0800

    add main1.java

---
+++ codes/0221-Maximal-Square/Java/main1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length, len = 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i) {
+        	for (int j = 1; j <= n; ++j) {
+        		if (matrix[i - 1][j - 1] == '1') {
+        			dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+        			if (dp[i][j] > len) {
+        				len = dp[i][j];
+        			}
+        		}
+        	}
+        }
+
+        return len * len;
+    }
+}
