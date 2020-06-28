41506c43b879d1bc1449670f8943773a79bd584a
Stackingrule <messageswitching@gmail.com>
2020-06-28 11:22:18 +0800

    add main1.cpp

---
+++ 剑指 Offer/剑指 Offer 42. 连续子数组的最大和/cpp/main1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    	if (nums.size() == 0) {
+    		return 0;
+    	}
+    	int res = nums[0];
+    	for (int i = 1; i < nums.size(); ++i) {
+    		nums[i] = nums[i] + max(nums[i - 1], 0);
+    		res = max(res, nums[i]);
+    	}
+    	return res;
+    }
+};
