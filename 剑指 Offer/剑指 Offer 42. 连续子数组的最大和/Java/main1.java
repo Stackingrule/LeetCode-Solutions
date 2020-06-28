3eaac30c98f94018623d97fa5133ab521de6f4a1
Stackingrule <messageswitching@gmail.com>
2020-06-28 11:24:45 +0800

    add main1.java

---
+++ 剑指 Offer/剑指 Offer 42. 连续子数组的最大和/Java/main1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+    	if (nums == null || nums.length == 0) {
+    		return 0;
+    	}
+    	int res = nums[0];
+    	for (int i = 1; i < nums.length; ++i) {
+    		nums[i] = nums[i] + Math.max(nums[i - 1], 0);
+    		res = Math.max(nums[i], res);
+    	}
+    	return res;
+    }
+}
