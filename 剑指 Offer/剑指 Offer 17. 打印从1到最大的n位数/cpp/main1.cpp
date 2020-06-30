129834e99459bb33fcf627ea13918806fef29c77
Stackingrule <messageswitching@gmail.com>
2020-06-30 22:31:40 +0800

    add cpp

---
+++ 剑指 Offer/剑指 Offer 17. 打印从1到最大的n位数/cpp/main1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> printNumbers(int n) {
+    	vector<int> res;
+    	if (n == 0) return res;
+
+    	for (int i = 1, max = pow(10, n); i < max; ++i) {
+    		res.push_back(i);
+    	}
+    	return res;
+    }
+};
