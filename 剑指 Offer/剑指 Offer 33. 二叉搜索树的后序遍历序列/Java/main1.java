76baad584c7cdf61cd0ee9d7daec4e82fe1f31d7
Stackingrule <messageswitching@gmail.com>
2020-06-30 19:02:47 +0800

    add Java

---
+++ 剑指 Offer/剑指 Offer 33. 二叉搜索树的后序遍历序列/Java/main1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean verifyPostorder(int[] postorder) {
+    	return checkTree(postorder, 0, postorder.length - 1);
+    }
+
+    public boolean checkTree(int[] postorder, int start, int end) {
+    	if (start >= end) {
+    		return true;
+    	}
+    	int root = postorder[end];
+    	int leftStart = start;
+    	int leftEnd = leftStart;
+
+    	while (leftStart < end && postorder[leftEnd] < root) {
+    		leftEnd++;
+    	}
+    	leftEnd--;
+
+    	int rightStart = leftEnd + 1;
+    	int rightEnd = end - 1;
+    	for (int i = rightStart; i < end; ++i) {
+    		if (postorder[i] > root)
+    			continue;
+    		return false;
+    	}
+    	 return checkTree(postorder, leftStart, leftEnd) && checkTree(postorder, rightStart, rightEnd);
+    }
+}
