82b6dd723c5791e7e9bdb91cc9c4d2f2b58ab4a5
Stackingrule <messageswitching@gmail.com>
2020-07-01 10:13:04 +0800

    add Java

---
+++ 剑指 Offer/剑指 Offer 14- II. 剪绳子 II/Java/main1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int cuttingRope(int n) {
+    	if (n <= 3) return n - 1;
+    	long res = 1;
+    	while (n > 4) {
+    		res = (res * 3) % 1000000007;
+    		n -= 3;
+    	}
+    	return (int) ((res * n) % 1000000007);
+    }
+}
