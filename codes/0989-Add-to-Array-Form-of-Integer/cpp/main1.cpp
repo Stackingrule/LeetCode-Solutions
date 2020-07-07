109459911bed79908ffa29b21ecb77876578ae1c
Stackingrule <messageswitching@gmail.com>
2020-07-07 22:05:16 +0800

    add cpp

---
+++ codes/0989-Add-to-Array-Form-of-Integer/cpp/main1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  vector<int> addToArrayForm(vector<int>& A, int K) {
+    vector<int> ans;
+    ans.reserve(A.size() + 1);    
+    for (int i = A.size() - 1; i >= 0 || K > 0; --i) {
+      K += (i >= 0 ? A[i] : 0);
+      ans.push_back(K % 10);
+      K /= 10;
+    }    
+    reverse(begin(ans), end(ans));
+    return ans;
+  }
+};
