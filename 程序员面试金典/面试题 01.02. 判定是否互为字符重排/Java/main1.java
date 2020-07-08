90ee49e56a19bc28ce220ee090a0626e97ad015c
Stackingrule <messageswitching@gmail.com>
2020-07-08 08:28:57 +0800

    add Java

---
+++ 程序员面试金典/面试题 01.02. 判定是否互为字符重排/Java/main1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean CheckPermutation(String s1, String s2) {
+        int l1=s1.length(),l2=s2.length();
+        if(l1!=l2)
+            return false;
+        int []index=new int[128];
+        for(int i=0;i<l1;i++){
+            index[s1.charAt(i)]++;
+            index[s2.charAt(i)]--;
+        }
+        for(int i=0;i<128;i++){
+            if(index[i]!=0)
+                return false;
+        }
+        return true;
+    }
+}
