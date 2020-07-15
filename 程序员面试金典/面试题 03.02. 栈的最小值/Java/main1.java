42420c950d255b20936eaadc95e6449a5d29c3c5
Stackingrule <messageswitching@gmail.com>
2020-07-15 14:09:28 +0800

    add Java

---
+++ 程序员面试金典/面试题 03.02. 栈的最小值/Java/main1.java
@@ -0,0 +1,49 @@
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+class MinStack {
+
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+        if (s2.isEmpty() || x <= s2.peek()) {
+            s2.push(x);
+        }
+    }
+    
+    public void pop() {
+        int x = s1.pop();
+        if (s2.peek() == x) {
+            s2.pop();
+        }
+    }
+    
+    public int top() {
+        return s1.peek();
+    }
+    
+    public int getMin() {
+        return s2.peek();        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
