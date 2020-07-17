eecb50eb356403399e909f3897dec45fcadbe7ca
Stackingrule <messageswitching@gmail.com>
2020-07-17 15:59:49 +0800

    add cpp

---
+++ 程序员面试金典/面试题 04.12. 求和路径/cpp/main1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        int res = 0;
+        vector<TreeNode*> out;
+        helper(root, sum, 0, out, res);
+        return res;
+    }
+
+private:
+	void helper(TreeNode* node, int sum, int curSum, vector<TreeNode*> &out, int &res) {
+		if (!node) return;
+		curSum += node->val;
+		out.push_back(node);
+		if (curSum == sum) ++res;
+		int t = curSum;
+		for (int i = 0; i < out.size() - 1; ++i) {
+			t -= out[i]->val;
+			if (t == sum) ++res;
+		}
+		helper(node->left, sum, curSum, out, res);
+		helper(node->right, sum, curSum, out, res);
+		out.pop_back();
+	}
+};
