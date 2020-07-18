58691c994d9894475c878d9fbd8c33e16b69708c
Stackingrule <messageswitching@gmail.com>
2020-07-18 09:24:19 +0800

    add cpp

---
+++ 程序员面试金典/面试题 10.01. 合并排序的数组/cpp/main1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+    	int index = m + n - 1;
+    	while (m > 0 && n > 0) {
+    		if (A[m - 1] > B[n - 1]) {
+    			A[index] = A[m - 1];
+    			--m;
+    		}
+    		else {
+    			A[index] = B[n - 1];
+    			--n;
+    		}
+    		--index;
+    	}
+    	while (n > 0) {
+    		A[index--] = B[n - 1];
+    		--n;
+    	}
+    }
+};
