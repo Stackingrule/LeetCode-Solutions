5b6167e3b5435633a3ca64948968222f39376028
Stackingrule <messageswitching@gmail.com>
2020-07-21 11:04:44 +0800

    add Java

---
+++ 程序员面试金典/面试题 16.25. LRU缓存/Java/main1.java
@@ -0,0 +1,44 @@
+class LRUCache {
+    private int capacity;
+    private HashMap<Integer,Integer> map;
+    private LinkedList<Integer> list;
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        map=new HashMap<>();
+        list=new LinkedList<>();
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            list.remove((Integer)key);
+            list.addLast(key);
+            return map.get(key);
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            list.remove((Integer)key);
+            list.addLast(key);
+            map.put(key,value);
+            return;
+        }
+        if(list.size()==capacity){
+            map.remove(list.removeFirst());
+            map.put(key,value);
+            list.addLast(key);
+        }
+        else{
+            map.put(key,value);
+            list.addLast(key);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
