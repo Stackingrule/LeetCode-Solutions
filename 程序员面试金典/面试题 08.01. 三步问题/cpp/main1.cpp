e4aeaf16baec0ab92f4b8714955165778649aa71
Stackingrule <messageswitching@gmail.com>
2020-07-22 21:34:26 +0800

    add cpp

---
+++ 程序员面试金典/面试题 08.01. 三步问题/cpp/main1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int waysToStep(int n) {
+        if(n  == 1) return 1;
+        else if(n == 2) return 2;
+        else if(n ==3 ) return 4;
+        int dp[n+1] ;
+        dp[1] = 1; dp[2] = 2; dp[3] = 4;
+        for(int i = 4; i <= n; i++ ) {
+            dp[i] = ((dp[i-3]  + dp[i-2] )% 1000000007 + dp[i-1]% 1000000007) % 1000000007;
+        }
+        return dp[n] ; 
+    }
+};
