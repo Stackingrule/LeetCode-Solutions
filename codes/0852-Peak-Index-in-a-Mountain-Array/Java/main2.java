99598058173723c433d12418c9fb2f59f590544b
Stackingrule <messageswitching@gmail.com>
2020-09-08 20:55:44 +0800

    add Java

---
+++ codes/0852-Peak-Index-in-a-Mountain-Array/Java/main2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length, left = 0, right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+}
