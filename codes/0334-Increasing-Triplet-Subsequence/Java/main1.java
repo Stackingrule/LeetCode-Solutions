8e5cc23012eff0236876aba506415d7aa964ff74
Stackingrule <messageswitching@gmail.com>
2020-09-10 11:41:19 +0800

    add Java

---
+++ codes/0334-Increasing-Triplet-Subsequence/Java/main1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (dp[i] >= 3) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
