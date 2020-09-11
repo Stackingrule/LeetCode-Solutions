b329cb9c58ad22922ba6b696ba7d7e7ef0149407
Stackingrule <messageswitching@gmail.com>
2020-09-11 15:39:08 +0800

    add cpp

---
+++ codes/0888-Fair-Candy-Swap/cpp/main1.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    vector<int> fairCandySwap(vector<int> &A, vector<int> &B)
+    {
+        int diff = (accumulate(A.begin(), A.end(), 0) - accumulate(B.begin(), B.end(), 0)) / 2;
+        unordered_set<int> st(A.begin(), A.end());
+        for (int num : B)
+        {
+            if (st.count(num + diff))
+                return {num + diff, num};
+        }
+        return {};
+    }
+};
