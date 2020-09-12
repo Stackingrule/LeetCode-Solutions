29cd8d79136b81fa033372dae51039c9d32481cb
Stackingrule <messageswitching@gmail.com>
2020-09-12 18:46:08 +0800

    add cpp

---
+++ codes/0542-01-Matrix/cpp/main2.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> res(m, vector<int>(n, INT_MAX - 1));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (matrix[i][j] == 0)
+                    res[i][j] = 0;
+                else
+                {
+                    if (i > 0)
+                        res[i][j] = min(res[i][j], res[i - 1][j] + 1);
+                    if (j > 0)
+                        res[i][j] = min(res[i][j], res[i][j - 1] + 1);
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; --i)
+        {
+            for (int j = n - 1; j >= 0; --j)
+            {
+                if (res[i][j] != 0 && res[i][j] != 1)
+                {
+                    if (i < m - 1)
+                        res[i][j] = min(res[i][j], res[i + 1][j] + 1);
+                    if (j < n - 1)
+                        res[i][j] = min(res[i][j], res[i][j + 1] + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
