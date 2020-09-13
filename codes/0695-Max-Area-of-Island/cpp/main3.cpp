934b0a863b3bff508d811176d6bbc8ba89ba328e
Stackingrule <messageswitching@gmail.com>
2020-09-13 12:00:13 +0800

    add cpp

---
+++ codes/0695-Max-Area-of-Island/cpp/main3.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int h = grid.size();
+        if (h == 0)
+            return 0;
+        int w = grid[0].size();
+
+        int ans = 0;
+        for (int i = 0; i < h; ++i)
+            for (int j = 0; j < w; ++j)
+                ans = max(ans, area(grid, j, i, w, h));
+        return ans;
+    }
+
+private:
+    int area(vector<vector<int>> &grid, int x, int y, int w, int h)
+    {
+        if (x < 0 || y < 0 || x >= w || y >= h || grid[y][x] == 0)
+            return 0;
+
+        grid[y][x] = 0;
+
+        return area(grid, x - 1, y, w, h) + area(grid, x + 1, y, w, h) + area(grid, x, y - 1, w, h) + area(grid, x, y + 1, w, h) + 1;
+    }
+};
