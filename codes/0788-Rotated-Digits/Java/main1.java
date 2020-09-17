3691f5a07296463fad53faad0302fa91a1b25ed8
Stackingrule <messageswitching@gmail.com>
2020-09-17 21:52:48 +0800

    add java

---
+++ codes/0788-Rotated-Digits/Java/main1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rotatedDigits(int N) {
+        int result = 0;
+    
+        for(int i=1; i<=N; i++){
+            int j=i;
+            boolean contains347 = false;
+            boolean contains2569 = false;
+            while(j!=0){
+                int mod = j%10;
+                if(mod==2||mod==5||mod==6||mod==9){
+                    contains2569=true;
+                }
+    
+                if(mod==3||mod==4||mod==7){
+                    contains347=true;
+                }
+    
+                j=j/10;
+            }
+    
+            if(!contains347&&contains2569){
+                result++;
+            }
+        }
+    
+        return result;
+    }
+}
