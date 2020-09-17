38f79978b851eb7cf8051aae5d0e7416903b5a00
Stackingrule <messageswitching@gmail.com>
2020-09-17 15:55:39 +0800

    rename directions

---
+++ codes/1545-Find-Kth-Bit-in-Nth-Binary-String/Java/main1.java
@@ -0,0 +1,11 @@
+class Solution {
+  public char findKthBit(int n, int k) {
+    if (n == 1) return '0';
+    int l = (1 << n) - 1;
+    if (k == (l + 1) / 2) return '1';
+    else if (k < (l + 1) / 2) 
+      return findKthBit(n - 1, k);
+    else
+      return (char)('1' - findKthBit(n - 1, l - k + 1) + '0');
+  }
+}
---
+++ codes/1545-Find-Kth-Bit-in-Nth-Binary-String/cpp/main1.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    char findKthBit(int n, int k)
+    {
+        if (n == 1) return '0';
+        int l = (1 << n) - 1;
+        if (k == (l + 1) / 2) return '1';
+        else if (k < (l + 1) / 2)
+        {
+            return findKthBit(n - 1, k);
+        }
+        else
+        {
+            return '1' - findKthBit(n - 1, l - k + 1) + '0';
+        }
+    }
+};
