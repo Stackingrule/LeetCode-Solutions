8206e22fef18bf54d7b89ce459da34c27724fcff
Stackingrule <messageswitching@gmail.com>
2020-09-22 21:38:16 +0800

    add java

---
+++ codes/1512-Number-of-Good-Pairs/Java/main2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int ans = 0;
+        int[] f = new int[101];
+        for (int i = 0; i < nums.length; ++i) {
+            ans += f[nums[i]]++;
+        }
+        return ans;
+    }
+}
