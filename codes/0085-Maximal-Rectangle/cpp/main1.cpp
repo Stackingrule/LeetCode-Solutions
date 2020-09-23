2f58bfc1c53b18a1458a82ee775a9ff3dd1afe07
Stackingrule <messageswitching@gmail.com>
2020-09-23 20:33:19 +0800

    add cpp

---
+++ codes/0085-Maximal-Rectangle/cpp/main1.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    int maximalRectangle(vector<vector<char>> &matrix)
+    {
+        int m = matrix.size();
+        if (m == 0)
+            return 0;
+        int n = matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                dp[i][j] = (matrix[i][j] == '1') ? (j == 0 ? 1 : dp[i][j - 1] + 1) : 0;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                int len = INT_MAX;
+                for (int k = i; k < m; ++k)
+                {
+                    len = min(len, dp[k][j]);
+                    if (len == 0)
+                        break;
+                    ans = max(len * (k - i + 1), ans);
+                }
+            }
+        }
+        return ans;
+    }
+};
