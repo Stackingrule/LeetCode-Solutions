a9e8287e0c252861f29b0d23c0917d0a270bc955
Stackingrule <messageswitching@gmail.com>
2020-09-24 14:57:14 +0800

    add Java

---
+++ codes/0933-Number-of-Recent-Calls/Java/main1.java
@@ -0,0 +1,27 @@
+class RecentCounter {
+
+    private Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.add(t);
+        while (q.size() > 0) {
+            int last = q.peek();
+            if (t - last > 3000) {
+                q.poll();
+            } else {
+                break;
+            }
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
