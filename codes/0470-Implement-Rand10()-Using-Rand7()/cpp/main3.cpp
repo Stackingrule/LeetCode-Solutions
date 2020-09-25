482f212d6cfb142319a205efaa9370f50d18efbf
Stackingrule <messageswitching@gmail.com>
2020-09-25 10:16:35 +0800

    add cpp

---
+++ codes/0470-Implement-Rand10()-Using-Rand7()/cpp/main3.cpp
@@ -0,0 +1,26 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution
+{
+public:
+    int rand10()
+    {
+        while (true)
+        {
+            int a = rand7(), b = rand7();
+            int num = (a - 1) * 7 + b;
+            if (num <= 40)
+                return num % 10 + 1;
+            a = num - 40, b = rand7();
+            num = (a - 1) * 7 + b;
+            if (num <= 60)
+                return num % 10 + 1;
+            a = num - 60, b = rand7();
+            num = (a - 1) * 7 + b;
+            if (num <= 20)
+                return num % 10 + 1;
+        }
+    }
+};
