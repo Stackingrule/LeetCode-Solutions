eb9f21f381c5866f445e97db25b35cd8ef93df57
Stackingrule <messageswitching@gmail.com>
2020-09-25 10:43:52 +0800

    add Java

---
+++ codes/0478-Generate-Random-Point-in-a-Circle/Java/main1.java
@@ -0,0 +1,20 @@
+class Solution {
+    double r, x, y;
+    public Solution(double radius, double x_center, double y_center) {
+        r = radius;
+        x = x_center;
+        y = y_center;
+    }
+    
+    public double[] randPoint() {
+        double len = Math.sqrt(Math.random()) * r;
+        double deg = Math.random() * 2 * Math.PI;
+        return new double[]{x + len * Math.cos(deg), y + len * Math.sin(deg)};
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(radius, x_center, y_center);
+ * double[] param_1 = obj.randPoint();
+ */
