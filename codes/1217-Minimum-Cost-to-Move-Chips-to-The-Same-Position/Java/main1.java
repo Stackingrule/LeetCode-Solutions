0bec905fc5eda9a5a8d18959ad6ffb0fa47b0cda
Stackingrule <messageswitching@gmail.com>
2020-09-26 08:22:53 +0800

    add Java

---
+++ codes/1217-Minimum-Cost-to-Move-Chips-to-The-Same-Position/Java/main1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0;
+        int even = 0;
+        for (int p : position) {
+            if (p % 2 == 0) {
+                ++odd;
+            } else {
+                ++even;
+            }
+        }
+        return Math.min(odd, even);
+    }
+}
