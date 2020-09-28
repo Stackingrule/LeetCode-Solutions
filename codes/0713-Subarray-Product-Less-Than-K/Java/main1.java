46daa698cf9674442e80a52fbd4121b0a34d4172
Stackingrule <messageswitching@gmail.com>
2020-09-28 18:52:20 +0800

    add Java

---
+++ codes/0713-Subarray-Product-Less-Than-K/Java/main1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int res = 0, prod = 1, left = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            prod *= nums[i];
+            while (left <= i && prod >= k) prod /= nums[left++];
+            res += i - left + 1;
+        }
+        return res;
+    }
+}
