fcba0d80e8a1fa11db88a981ba78cc9012add0b1
Stackingrule <messageswitching@gmail.com>
2020-09-28 08:07:59 +0800

    add cpp

---
+++ codes/0657-Robot-Return-to-Origin/cpp/main1.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool judgeCircle(string moves)
+    {
+        int w = 0, h = 0;
+        for (int i = 0; i < moves.length(); i++)
+            if (moves[i] == 'L')
+                w++;
+            else if (moves[i] == 'R')
+                w--;
+            else if (moves[i] == 'U')
+                h++;
+            else
+                h--;
+        return w == 0 && h == 0;
+    }
+};
