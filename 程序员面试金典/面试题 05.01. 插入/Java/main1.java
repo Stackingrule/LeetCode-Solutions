0d7ff6274a570bfcdc38f2467dc163d47b6b44e4
Stackingrule <messageswitching@gmail.com>
2020-11-14 20:29:07 +0800

    add Java

---
+++ 程序员面试金典/面试题 05.01. 插入/Java/main1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int insertBits(int N, int M, int i, int j) {
+        int mask = ((1 << (j - i + 1)) - 1) << i;
+        mask = ~mask;
+        N &= mask;
+        M = M << i;
+        return M | N;
+    }
+}
