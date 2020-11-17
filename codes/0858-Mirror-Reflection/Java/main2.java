a080482392697b07e85d73ce449c0b7c2f6f86ee
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-17 16:18:16 +0800

    add Java

---
+++ codes/0858-Mirror-Reflection/Java/main2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        return 1 - p / gcd(p, q) % 2 + q / gcd(p, q) % 2;
+    }
+
+    private int gcd(int p, int q) {
+        return q != 0 ? gcd(q, p % q) : p;
+    }
+}
