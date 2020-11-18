b6f96ce49275d0f8866c5a1cdd693daf52746d07
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-18 20:20:41 +0800

    add Java

---
+++ codes/0413-Arithmetic-Slices/Java/main2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int res = 0, n= A.length;
+        int[] dp = new int[n];
+        for (int i = 2; i < n; ++i) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+            res += dp[i];
+        }
+        return res;
+    }
+}
