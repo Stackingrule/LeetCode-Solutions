173f168dccc56e0f52d652461aef5def1ba50375
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-23 20:59:06 +0800

    add Java

---
+++ 程序员面试金典/面试题 10.02. 变位词组/Java/main1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(strs);
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortStr = String.valueOf(chars);
+            if (!map.containsKey(sortStr)) {
+                map.put(sortStr, new ArrayList<>());
+            }
+            map.get(sortStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
