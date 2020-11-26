e25ea06f21e5cb258e2d0ea958202010428ffd48
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-26 09:57:07 +0800

    add cpp

---
+++ codes/0164-Maximum-Gap/cpp/main1.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int maximumGap(vector<int> &nums)
+    {
+        if (nums.size() <= 1)
+            return 0;
+        int mx = INT_MIN, mn = INT_MAX, n = nums.size(), pre = 0, res = 0;
+        for (int num : nums)
+        {
+            mx = max(mx, num);
+            mn = min(mn, num);
+        }
+        int size = (mx - mn) / n + 1, cnt = (mx - mn) / size + 1;
+        vector<int> bucket_min(cnt, INT_MAX), bucket_max(cnt, INT_MIN);
+        for (int num : nums)
+        {
+            int idx = (num - mn) / size;
+            bucket_min[idx] = min(bucket_min[idx], num);
+            bucket_max[idx] = max(bucket_max[idx], num);
+        }
+        for (int i = 1; i < cnt; ++i)
+        {
+            if (bucket_min[i] == INT_MAX || bucket_max[i] == INT_MIN)
+                continue;
+            res = max(res, bucket_min[i] - bucket_max[pre]);
+            pre = i;
+        }
+        return res;
+    }
+};
