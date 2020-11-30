72bd0da15a96c4cdb5ab682054f9187f1d3cd743
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-30 15:30:21 +0800

    add cpp

---
+++ codes/0503-Next-Greater-Element-II/cpp/main1.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> res(n, -1);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < i + n; ++j)
+            {
+                if (nums[j % n] > nums[i])
+                {
+                    res[i] = nums[j % n];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
