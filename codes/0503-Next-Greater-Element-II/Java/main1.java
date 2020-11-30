7784deb0f44b9ba8444a036642c6856dcee4b097
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-11-30 15:47:04 +0800

    add Java

---
+++ codes/0503-Next-Greater-Element-II/Java/main1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < i + n; ++j) {
+                if (nums[j % n] > nums[i]) {
+                    ans[i] = nums[j % n];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
