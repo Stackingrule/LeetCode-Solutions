9b8486f89cd71b18fa3e1d99e29c363fa30fd6fc
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-02 17:29:21 +0800

    add Java

---
+++ codes/0382-Linked-List-Random-Node/Java/main1.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    ListNode head;
+    Random random;
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+        this.random = new Random();
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        ListNode iter = this.head;
+        int res = iter.val;
+
+        iter = iter.next;
+        int n = 1;
+        while(iter != null) {
+            ++n;
+            if(random.nextInt(n) == 0) {
+                res = iter.val;
+            }
+            iter = iter.next;
+        }
+        return res;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
