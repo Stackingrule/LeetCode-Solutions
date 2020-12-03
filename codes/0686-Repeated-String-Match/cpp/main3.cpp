ff10ea9a5dd06b3df31e3a2dfb4b6b7a2582ae75
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-03 15:27:08 +0800

    add cpp

---
+++ codes/0686-Repeated-String-Match/cpp/main3.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int repeatedStringMatch(string A, string B)
+    {
+        string t = A;
+        for (int i = 1; i <= B.size() / A.size() + 2; ++i)
+        {
+            if (t.find(B) != string::npos)
+                return i;
+            t += A;
+        }
+        return -1;
+    }
+};
