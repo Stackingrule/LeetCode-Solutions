79b1616407811c9936c96f8c289edd695396ac74
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-03 14:59:32 +0800

    add cpp

---
+++ codes/0678-Valid-Parenthesis-String/cpp/main1.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    bool checkValidString(string s)
+    {
+        int left = 0, right = 0, n = s.size();
+        for (int i = 0; i < n; ++i)
+        {
+            if (s[i] == '(' || s[i] == '*')
+                ++left;
+            else
+                --left;
+            if (left < 0)
+                return false;
+        }
+        if (left == 0)
+            return true;
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (s[i] == ')' || s[i] == '*')
+                ++right;
+            else
+                --right;
+            if (right < 0)
+                return false;
+        }
+        return true;
+    }
+};
