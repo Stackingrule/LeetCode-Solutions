2c9655b64eddea66b5838bf9229e1637969ce87f
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-05 19:15:23 +0800

    add cpp

---
+++ codes/0892-Surface-Area-of-3D-Shapes/cpp/main1.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int surfaceArea(vector<vector<int>> &grid)
+    {
+        int n = grid.size(), ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] > 0)
+                    ans += 4 * grid[i][j] + 2;
+                if (i > 0)
+                    ans -= min(grid[i][j], grid[i - 1][j]) * 2;
+                if (j > 0)
+                    ans -= min(grid[i][j], grid[i][j - 1]) * 2;
+            }
+        }
+        return ans;
+    }
+};
