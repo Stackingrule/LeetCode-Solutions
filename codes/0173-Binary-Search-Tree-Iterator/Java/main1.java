34a01b60d6eaf893f30c4d7f0984fe69b3f267b1
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-09 16:32:18 +0800

    add Java

---
+++ codes/0173-Binary-Search-Tree-Iterator/Java/main1.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class BSTIterator {
+
+    private TreeNode cur;
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        cur = root;
+        stack = new Stack<>();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        int val = cur.val;
+        cur = cur.right;
+        return val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        if (!stack.isEmpty() || cur != null) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such: BSTIterator
+ * obj = new BSTIterator(root); int param_1 = obj.next(); boolean param_2 =
+ * obj.hasNext();
+ */
