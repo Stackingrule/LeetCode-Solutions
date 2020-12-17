a352f5161263cf448fd7eb474554a635435d3093
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-17 14:47:30 +0800

    add cpp

---
+++ codes/1417-Reformat-The-String/cpp/main1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reformat(string s) {
+
+        string a, b;
+        for(char c: s)
+            if(c >= '0' && c <= '9') a += c;
+            else b += c;
+
+        if(abs((int)a.size() - (int)b.size()) >= 2) return "";
+
+        if(a.size() < b.size()) swap(a, b);
+        string res = "";
+        for(int i = 0; i < b.size(); i ++){
+            res += a[i];
+            res += b[i];
+        }
+
+        if(a.size() > b.size()) res += a.back();
+        return res;
+    }
+};
