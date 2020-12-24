5ec3439281ff08ee7b3d9bc7ed62e9222c8c3c74
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-24 11:54:48 +0800

    add cpp

---
+++ codes/1624-Largest-Substring-Between-Two-Equal-Characters/cpp/main1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int maxLengthBetweenEqualCharacters(string s) {
+    vector<int> first(26, -1);
+    int ans = -1;
+    for (int i = 0; i < s.length(); ++i) {
+      int& p = first[s[i] - 'a'];
+      if (p != -1) {
+        ans = max(ans, i - p - 1);
+      } else {
+        p = i;
+      }      
+    }
+    return ans;
+  }
+};
