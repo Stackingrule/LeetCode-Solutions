b7de6fafed7d9ac7bb816a9b1a35e01ae4248222
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-26 14:26:27 +0800

    add cpp

---
+++ codes/1424-Diagonal-Traverse-II/cpp/main1.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>> &nums)
+    {
+        vector<vector<int>> m;
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = 0; j < nums[i].size(); ++j)
+            {
+                if (m.size() <= i + j)
+                    m.push_back({});
+                m[i + j].push_back(nums[i][j]);
+            }
+        vector<int> ans;
+        for (const auto &d : m)
+            ans.insert(end(ans), rbegin(d), rend(d));
+        return ans;
+    }
+};
