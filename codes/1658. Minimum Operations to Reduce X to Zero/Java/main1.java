baecae8bf7285a0fda1e563f8ae6a3e0c630dfde
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2021-01-14 19:00:04 +0800

    add Java

---
+++ codes/1658. Minimum Operations to Reduce X to Zero/Java/main1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length, total = 0;
+        for (int num : nums)
+            total += num;
+
+        int target = total - x;
+        if (target < 0)
+            return -1;
+        if (target == 0)
+            return n;
+
+        int left = 0, right = 0, sum = 0, ans = -1;
+        while (right < n) {
+            if (sum < target) {
+                sum += nums[right];
+                right++;
+            }
+            while (sum >= target) {
+                if (sum == target) {
+                    ans = Math.max(ans, right - left);
+                }
+                sum -= nums[left];
+                left++;
+            }
+        }
+        if (ans == -1) {
+            return -1;
+        }
+        return n - ans;
+    }
+}
