0aa9c0da08e7d01e0c7c70e377f867fbd2a5005a
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2021-02-05 09:29:44 +0800

    add cpp

---
+++ codes/1208. Get Equal Substrings Within Budget/cpp/main1.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int equalSubstring(string s, string t, int maxCost)
+    {
+        int j = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            maxCost -= abs(s[i] - t[i]);
+            if (maxCost < 0)
+            {
+                maxCost += abs(s[j] - t[j++]);
+            }
+        }
+        return s.length() - j;
+    }
+};
