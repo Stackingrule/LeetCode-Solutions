6316ec5f507957e14e2b52cfdac170a1cbddd11d
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2021-02-20 16:37:00 +0800

    add cpp

--- codes/0926. Flip String to Monotone Increasing/cpp/main1.cpp
+++ codes/0926. Flip String to Monotone Increasing/cpp/main1.cpp
@@ -5,7 +5,7 @@ public:
     {
         int n = S.size(), ans = INT_MAX;
         vector<int> cnt1(n + 1), cnt0(n + 1);
-        for (int i = 1, j - n - 1; j >= 0; ++i, --j)
+        for (int i = 1, j = n - 1; j >= 0; ++i, --j)
         {
             cnt1[i] = cnt1[i - 1] + (S[i - 1] == '0' ? 0 : 1);
             cnt0[j] += cnt0[j + 1] + (S[j] == '1' ? 0 : 1);
