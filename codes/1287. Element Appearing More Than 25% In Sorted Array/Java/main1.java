8e81340734da461f6f09db682eee499043567fe0
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2021-02-25 21:16:44 +0800

    add Java

--- codes/1287. Element Appearing More Than 25% In Sorted Array/Java/main1.java
+++ codes/1287. Element Appearing More Than 25% In Sorted Array/Java/main1.java
@@ -1,8 +1,9 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
         int n = arr.length / 4;
-        for (int i = 0; i + s < arr.size(); ++i)
-        if (arr[i] == arr[i + s]) return arr[i];
+        for (int i = 0; i + n < arr.length; ++i)
+            if (arr[i] == arr[i + n])
+                return arr[i];
         return -1;
     }
 }
