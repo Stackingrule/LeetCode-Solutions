2ed47c9b9a4c777f28130076c58a04899b331775
Stackingrule <messageswitching@gmail.com>
2021-07-04 09:23:39 +0800

    fixed error

--- codes/0278-First-Bad-Version/Java/main1.java
+++ codes/0278-First-Bad-Version/Java/main1.java
@@ -5,7 +5,7 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1, right = n;
         while (left < right) {
-        	int mid = left + (right - left);
+        	int mid = left + (right - left) / 2;
         	if (isBadVersion(mid)) right = mid;
         	else left = mid + 1;
         }
